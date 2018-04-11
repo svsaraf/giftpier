dfec65299d116c807cb13bbfad984a463cc5f48d
Sanjay Saraf <svsaraf90@gmail.com>
2018-04-11 12:53:23 -0400

    added models successfully

--- azsearch/admin.py
+++ azsearch/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+import models
+
+admin.site.register(models.Gift)
+
 # Register your models here.
--- azsearch/models.py
+++ azsearch/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Gift(models.Model):
+    link = models.CharField(max_length=128)
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
 # Create your models here.
